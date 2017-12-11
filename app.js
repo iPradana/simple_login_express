6c56b9beb557c3f2ec7586d914551db1b4d9c4b1
Indra Pradana A <iPradana7@gmail.com>
2017-12-11 09:53:53 +0700

    Add files via upload
    views for login and register

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express')
+var app = express();
+var dpath = require("path");
+var router = express.Router();
+var path = __dirname + '/views/';
+var session = require('express-session');
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(dpath.join(__dirname, 'views')));
+
+app.get("/",function(req,res){
+  res.render('index');
+});
+
+app.get("/register",function(req,res){
+  res.render('register');
+});
+
+app.use("*",function(req,res){
+  res.render('index.ejs');
+});
+
+
+app.listen(80,function(){
+  console.log("Live at Port 80");
+});
